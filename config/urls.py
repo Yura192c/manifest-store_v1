39a50b3762da3ad4239d16e8d304fba3e28f959b
Yura192c <fpm.mironchiYU@bsu.by>
2023-06-06 00:37:44 +0300

    add urls for shop, cart, main apps

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import  settings
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('src.main.urls')),
+
+    path('cart/', include(('src.cart.urls', 'cart'), namespace='cart')),
+    path('shop/', include(('src.shop.urls', 'shop'), namespace='shop')),
 ]
 
 if settings.DEBUG:
     urlpatterns = [
-        path('__debug__/', include('debug_toolbar.urls')),
-    ] + urlpatterns
+                      path('__debug__/', include('debug_toolbar.urls')),
+                  ] + urlpatterns
     # urlpatterns += static(settings.MEDIA_URL,
     #                       document_root=settings.MEDIA_ROOT)
