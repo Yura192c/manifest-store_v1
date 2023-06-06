698b7acfb9f42e0af92daf43560a04d2ffcad7bf
Yura192c <fpm.mironchiYU@bsu.by>
2023-06-07 01:11:44 +0300

    add correct translate for product description

--- src/shop/parser/parser.py
+++ src/shop/parser/parser.py
@@ -7,7 +7,7 @@ from fake_useragent import UserAgent
 ua = UserAgent()
 translator = Translator(to_lang="ru")
 headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'User-Agent': ua.random
 }
 
@@ -48,10 +48,12 @@ def parse_NB(url_to_parse):
                                   .find('div', class_='price') \
                                   .find('span', class_='price-value') \
                                   .find('span').get_text().translate({ord(i): None for i in '\n'}).replace('$', ''))
-    descroption = soup.find('div', class_='col-12 value content short-description px-0 pt-6 pt-lg-4 pb-3') \
+    description = soup.find('div', class_='col-12 value content short-description px-0 pt-6 pt-lg-4 pb-3') \
         .get_text().translate({ord(i): None for i in '\n'}).replace('Description', '')
-    product_data['description'] = translator.translate(descroption)
 
+    translated_description = [translator.translate(desc) for desc in description.split('.')]
+    final_str = ''.join(list(map(lambda x: x + '. ', translated_description)))
+    product_data['description'] = final_str
     sizes = soup.find_all('div', class_='row dyanamic-attr')[0].find_all('button')
     size = []
     for s in sizes:
@@ -68,6 +70,8 @@ def parse_NB(url_to_parse):
         except:
             pass
     product_data['images'] = images
+    product_data['color'] = translator.translate(
+        soup.find('div', class_='attributes').find('span', class_='display-color-name').get_text())
     return product_data
 
 
