dae043c7c45e5fa457de0c0962f5ef38b1bf6d3f
Yura192c <fpm.mironchiYU@bsu.by>
2023-06-08 00:41:29 +0300

    refactoring code

--- src/shop/views.py
+++ src/shop/views.py
@@ -1,76 +1,79 @@
-from django.views.generic import ListView, DetailView
-from .forms import SearchForm
-
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import Category, Product
-from src.cart.forms import CartAddProductForm
-from .choices import SORTING_VALUES
-
-# from cart.forms import CartAddProductForm
+# Standard library imports
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import QuerySet
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView
+from typing import Any, Dict, Union, Optional
+
+# Third-party imports
+
+# Local application/library imports
+from .forms import SearchForm
+from .models import Category, Product
+from .choices import SORTING_VALUES
+from src.cart.forms import CartAddProductForm
 
 
-def product_list(request):
-    """Список Товаров"""
-    data = filters(request)
-    return render(request,
-                    'shop/list.html',
-                  data)
+def product_list(request: HttpRequest) -> HttpResponse:
+    """List of products"""
+    data: Dict[str, Any] = filters(request)
+    return render(
+        request,
+        'shop/list.html',
+        data
+    )
 
 
-def product_detail(request, category_slug=None, id=None, slug=None):
-    product = get_object_or_404(Product,
-                                id=id,
-                                slug=slug,
-                                available=True)
+def product_detail(request: HttpRequest, category_slug: Optional[str] = None,
+                   pk: Optional[int] = None, slug: Optional[str] = None) -> HttpResponse:
+    """Product details"""
+    product = get_object_or_404(Product, id=pk, slug=slug, available=True)
     cart_product_form = CartAddProductForm()
-    return render(request,
-                  'shop/detail.html',
-                  {'product': product,
-                   'cart_product_form': cart_product_form
-                   })
+    return render(
+        request,
+        'shop/detail.html',
+        {
+            'product': product,
+            'cart_product_form': cart_product_form,
+        }
+    )
 
 
-def filters(request):
-    data = {}
-    search = request.GET.get('search', None)
-    sort = request.GET.get('sort', None)
-    page_number = request.GET.get('page', None)
-    gender = request.GET.get('gender', None)
+def filters(request: HttpRequest) -> Dict[str, Union[Product, str, Optional[str], Dict[str, Any], Paginator]]:
+    """Filters for products"""
+    data: Dict[str, Union[Product, str, Optional[str], Dict[str, Any], Paginator]] = {}
+    search: Optional[str] = request.GET.get('search')
+    sort: Optional[str] = request.GET.get('sort')
+    page_number: Optional[str] = request.GET.get('page')
+    gender: Optional[str] = request.GET.get('gender')
 
-    if search is not None:
-        if gender is not None:
-            products = Product.objects.filter(available=True, name__icontains=search, gender=gender)
-        else:
-            products = Product.objects.filter(available=True, name__icontains=search)
+    products: QuerySet[Product] = Product.objects.filter(available=True)
 
-    else:
-        if gender is not None:
-            products = Product.objects.filter(available=True, gender=gender)
-        else:
-            products = Product.objects.filter(available=True)
+    if search:
+        products = products.filter(name__icontains=search)
+    if gender:
+        products = products.filter(gender=gender)
 
-    if sort is not None:
-        if sort == 'price_low':
-            products = products.order_by('price')
-        elif sort == 'price_high':
-            products = products.order_by('-price')
-        elif sort == 'name_a_z':
-            products = products.order_by('name')
-        elif sort == 'name_z_a':
-            products = products.order_by('-name')
+    sorting_options: Dict[str, str] = {
+        'price_low': 'price',
+        'price_high': '-price',
+        'name_a_z': 'name',
+        'name_z_a': '-name',
+    }
 
-    paginator = Paginator(products, 9)
+    if sort:
+        sort_key: Optional[str] = sorting_options.get(sort)
+        if sort_key:
+            products = products.order_by(sort_key)
 
+    paginator: Paginator = Paginator(products, 9)
     page_obj = paginator.get_page(page_number)
+
     try:
         products = paginator.page(page_number)
-    except PageNotAnInteger:
-        # If page is not an integer deliver the first page
+    except (PageNotAnInteger, EmptyPage):
         products = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range deliver last page of results
-        products = paginator.page(paginator.num_pages)
 
     data['products'] = products
     data['search'] = search
