1ef4d6304b225e1b8ea178d48e0335101dee6dab
Yura192c <fpm.mironchiYU@bsu.by>
2023-06-13 01:32:23 +0300

    add order app: models, views

--- src/order/models.py
+++ src/order/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from src.shop.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    first_name = models.CharField('Имя',max_length=50)
+    last_name = models.CharField('Фамилия',max_length=50)
+    email = models.EmailField('Почта')
+    address = models.CharField('Адрес доставки',max_length=250)
+    postal_code = models.CharField('Почтовый индекс',max_length=20)
+    city = models.CharField('Город',max_length=100)
+    country = models.CharField('Страна',max_length=100)
+    created = models.DateTimeField('Обновлен',auto_now_add=True)
+    updated = models.DateTimeField('Создан',auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created']
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+
+    def __str__(self):
+        return f'Заказ {self.id}'
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,
+                              related_name='items',
+                              on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,
+                                related_name='order_items',
+                                on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    size = models.CharField(max_length=10, default='-')
+
+    class Meta:
+        verbose_name = 'Товар в заказе'
+        verbose_name_plural = 'Товары в заказе'
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
--- src/order/views.py
+++ src/order/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import OrderItem
+from .forms import OrderCreateForm
+from src.cart.cart import Cart
 
-# Create your views here.
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                         product=item['product'],
+                                         price=item['price'],
+                                         size=item['size'],
+                                         quantity=item['quantity'])
+            # очистить корзину
+            cart.clear()
+            return render(request,
+                          'order/created.html',
+                          {'order': order})
+    else:
+        form = OrderCreateForm()
+    return render(request,
+                  'order/create.html',
+                  {'cart': cart, 'form': form})
