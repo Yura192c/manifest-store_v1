7d0832e04cadf5adeb33c25a8b39117941775e27
Yura192c <fpm.mironchiYU@bsu.by>
2023-06-13 01:31:23 +0300

    refactoring code

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
-    path('<slug:category_slug>/<int:id>/<slug:slug>/', views.product_detail,
+    path('<slug:category_slug>/<int:pk>/<slug:slug>/', views.product_detail,
          name='product_detail'),
 ]
